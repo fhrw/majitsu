52a20c758cd76caf6f4e57971f44680bda024197
Felix Watson <felix.dork@gmail.com>
2025-10-18 12:34:32 +1100

    majitsu process

---
+++ majitsu-process.el
@@ -0,0 +1,26 @@
+(defcustom majitsu-jj-executable
+  (executable-find "jj")
+  "The JJ executable used on the local host"
+  :package-version '(majitsu . "0.1")
+  :group 'majitsu-process
+  :type 'string)
+
+(defun majitsu--call (&rest args)
+  "Run jj with ARGS and return its stdout as a string."
+  (with-current-buffer (majitsu--process-buffer)
+    (erase-buffer)
+    (apply #'process-file majitsu-jj-executable nil (current-buffer) nil args)
+    (string-trim (buffer-string))))
+
+(defun majitsu--lines (&rest args)
+  "Return jj output as a list of lines."
+  (split-string (apply #'majitsu--call args) "\n" t))
+
+(defvar majitsu--process-buffer "*majitsu*"
+  "Buffer name for Majitsu process output.")
+
+(defun majitsu--process-buffer ()
+  (get-buffer-create majitsu--process-buffer))
+
+(provide 'majitsu-process)
+;;; majitsu-process.el ends here
