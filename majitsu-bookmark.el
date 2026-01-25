3590f1b28bca0bb471bb4ca39a418555375cdba9
Felix Watson <felix.dork@gmail.com>
2026-01-25 17:05:24 -0500

    push bookmark command

--- majitsu-bookmark.el
+++ majitsu-bookmark.el
@@ -5,17 +5,22 @@
 ;; Interactive Commands ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun majitsu-rebase-bookmark-interactive ()
+(defun majitsu-rebase-bookmark ()
   "Prompt for the SELECTED bookmark and then rebase it and all parent commits onto TARGET."
   (interactive)
   (let ((selected (majitsu--prompt-for-bookmark "Bookmark to rebase: "))
 	(target (majitsu--prompt-for-bookmark "Bookmark to rebase onto: ")))
     (majitsu--rebase-bookmark (plist-get selected :name) (plist-get target :name))))
 
-(defun majitsu-move-bookmark-to-current-interactive ()
+(defun majitsu-move-bookmark-to-current ()
   "Prompt for a bookmark and move it to the current position."
   (interactive)
   (majitsu--move-bookmark (plist-get (majitsu--prompt-for-bookmark "Bookmark to move: ") :name)))
+
+(defun majitsu-push-bookmark ()
+  "Prompt for a bookmark and push it to the remote."
+  (interactive)
+  (majitsu--push-bookmark (majitsu--prompt-for-bookmark "Bookmark to push: ")))
 
 ;;;;;;;;;;;;;;;
 ;; Functions ;;
@@ -41,6 +46,10 @@
 (defun majitsu--move-bookmark (name)
   "move the bookmake NAME to current revision"
   (majitsu--call "b" "m" name "--allow-backwards"))
+
+(defun majitsu--push-bookmark (bookmark)
+  "Push the BOOKMARK to the remote."
+  (majitsu--call "git" "push" "-b" (plist-get bookmark :name)))
 
 (defun majitsu--rebase-bookmark (selected target)
   "rebase SELECTED bookmark (whole branch) onto TARGET"
